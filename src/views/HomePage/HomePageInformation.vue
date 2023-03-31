5862202c0e5ba82d24f767938539f2e2763484fe
spacedawwwg <spacedawwwg@gmail.com>
2023-03-31 21:47:04 +0100

    update content

--- src/views/HomePage/HomePageInformation.vue
+++ src/views/HomePage/HomePageInformation.vue
@@ -3,20 +3,20 @@
     <p>Across the weekend participants will have the opportunity to take part in:</p>
     <h3>Friday</h3>
     <ul>
-      <li><strong>Kings of War Ambush:</strong> 4:30pm to 9pm, 800pts, 4 games</li>
-      <li><strong>Firefight:</strong> 4.30pm to 9pm, 500pts, 4 games</li>
+      <li><strong>Kings of War Ambush</strong> - 4:30pm to 9pm, 800pts, 4 games</li>
+      <li><strong>Firefight</strong> - 4.30pm to 9pm, 500pts, 4 games</li>
     </ul>
     <h3>Saturday</h3>
     <ul>
-      <li><strong>Kings of War singles:</strong> 9am to 6pm, 2300pts, 3 games</li>
-      <li><strong>Firefight (1500pts):</strong> 9am to 6pm, 1500pts, 3 games</li>
-      <li><strong>Firefight "Big Tank Bash":</strong> 7.30pm to 10pm, Participation Game</li>
+      <li><strong>Kings of War singles</strong> - 9am to 6pm, 2300pts, 3 games</li>
+      <li><strong>Firefight</strong> - 9am to 6pm, 1500pts, 3 games</li>
+      <li><strong>Firefight "Big Tank Bash"</strong> - 7.30pm to 10pm, Participation Game</li>
     </ul>
     <h3>Sunday</h3>
     <ul>
-      <li><strong>Kings of War doubles:</strong> 9am to 6pm, 2000pts, 3 games</li>
-      <li><strong>Deadzone:</strong> 9am to 6pm, Details tbc</li>
-      <li><strong>Armada:</strong> 9am to 6pm, Details tbc</li>
+      <li><strong>Kings of War doubles</strong> - 9am to 6pm, 2000pts, 3 games</li>
+      <li><strong>Deadzone</strong> - 9am to 6pm, Details tbc</li>
+      <li><strong>Armada</strong> - 9am to 6pm, Details tbc</li>
     </ul>
 
     <!-- <VButton href="/files/NKGT-2023_EventPack_v1.0.pdf" :variants="['decorative']">
